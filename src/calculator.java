2da41776e5dae1bbc8e533fbfe9bebd95b8ba1f0
sajjadmahmood <sajjadmahmood979@yahoo.com>
2019-08-02 08:01:19 -0400

    uploadingmyhomework

--- src/calculator.java
+++ src/calculator.java
@@ -4,13 +4,41 @@ public class calculator {
 
 	public static void main(String[] args) {
 		Scanner sajjad = new Scanner (System.in);
-		double age, secondage, answer;
+
+
+		//declaring variables
+		long age, secondage;
+		long ageDifference;
+		String myName = "Sajjad";
+		String mG = "Angelina Julie";
+
+//inputing your own age
 		System.out.println("Enter your age");
-		age = sajjad.nextDouble();
+
+//assigning age enetered into a variable
+		age = sajjad.nextLong();
+
+//same thing for the second/girlfriend's age
 		System.out.println("Enter your girlfriend age");
-		secondage = sajjad.nextDouble();
-		answer = age / secondage;
-		System.out.println(answer);
+		secondage = sajjad.nextLong();
+
+
+//declared boolean to enter the conditional statment
+		boolean comparison1 = age <= secondage;
+		boolean comparison2 = age > secondage;
+
+//creating conditional if/else statment
+		if(comparison1){
+			System.out.println(mG + " is either older than you are or same age as you!");
+		}else if(comparison2){
+			System.out.println("Good to hear!! have fun " + myName + "!");
+		}
+
+
+//getting the remainder. the age difference
+		ageDifference = age - secondage;
+//printing out the results
+		System.out.println("Age difference is " + ageDifference);
 		
 		
 		
